cad11577d024988a8e502e0dd7edcc6538440a40
Jessie Chen <quansu@outlook.com>
2022-11-06 22:25:31 +0000

    Update axios.js

--- CatLover/src/axios.js
+++ CatLover/src/axios.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // THE API (cloud function) URL
   baseURL: 'https://us-central1-challenge-9e9bc.cloudfunctions.net/api'
-  // 'http://localhost:5001/challenge-9e9bc/us-central1/api'
-  // 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
-    // "http://localhost:5001/challenge-4b2b2/us-central1/api",
+ 
 });
 
 export default instance;
